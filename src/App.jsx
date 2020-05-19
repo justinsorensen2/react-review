06ab5102ec267a8c26fa021d4d871e98d0ebe2ab
justinsorensen2dotnet <>
2020-05-19 17:46:22 -0400

    added basic render ternary to hide component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,32 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
-import Page from './pages/Page'
-import Page2 from './pages/Page2'
-import HomePage from './pages/HomePage'
-import NotFound from './pages/NotFound'
 import Counter from './components/Counter'
 import ImageSlider from './components/ImageSlider'
 
-const App = () => {
-  return (
-    <Router>
-      <header>
-        <h1>Welcome to my SPA</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go Home</Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Counter />
+class App extends React.Component {
+  state = {
+    visible: true,
+  }
 
-      <ImageSlider />
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/1" component={Page}></Route>
-        <Route exact path="/2" component={Page2}></Route>
-        <Route path="*" component={NotFound}></Route>
-      </Switch>
-    </Router>
-  )
+  render() {
+    const buttonText = this.state.visible
+      ? 'HIDE IMG SLIDER'
+      : 'SHOW IMG SLIDER'
+
+    return (
+      <>
+        <Counter />
+
+        {this.state.visible ? <ImageSlider /> : null}
+        <button
+          onClick={() => {
+            this.setState({ visible: !this.state.visible })
+          }}
+        >
+          {buttonText}
+        </button>
+      </>
+    )
+  }
 }
 
 export default App
